ae57869d06a9c310d6794e0576d3ed5ed83ba412
rizkasalisaputeri <rizka.salisa.puteri-2021@vokasi.unair.ac.id>
2025-06-08 20:08:30 +0700

    Add serve.py for local serving

---
+++ MLProject/serve.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import mlflow.pyfunc
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Muat model dengan run_id yang benar
+model = mlflow.pyfunc.load_model("runs:/36cb9a86397340d7b8af4897fc55a54d/model")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    df = pd.DataFrame(data)
+    prediction = model.predict(df)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
