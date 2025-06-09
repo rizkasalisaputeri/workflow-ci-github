e4bb4bca6f9db92f68ff6949d39eb51f0ea73275
rizkasalisaputeri <rizka.salisa.puteri-2021@vokasi.unair.ac.id>
2025-06-09 22:04:57 +0700

    Remove --verbose from mlflow run and fix CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,52 +7,42 @@ import mlflow
 import mlflow.sklearn
 import logging
 
-# Konfigurasi logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Load preprocessed dataset
 df = pd.read_csv("heart_processed.csv")
 X = df.drop('HeartDisease', axis=1)
 y = df['HeartDisease']
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Inisialisasi model
 model = RandomForestClassifier(random_state=42)
 
-# Mulai MLflow run
 with mlflow.start_run(run_name="RandomForest_Basic"):
     try:
-        # Latih model
         logger.info("Training model...")
         model.fit(X_train, y_train)
         
-        # Prediksi
         logger.info("Making predictions...")
         y_pred = model.predict(X_test)
         
-        # Hitung metrik
         accuracy = accuracy_score(y_test, y_pred)
         precision = precision_score(y_test, y_pred)
         recall = recall_score(y_test, y_pred)
         f1 = f1_score(y_test, y_pred)
         
-        # Log metrik
         logger.info("Logging metrics...")
         mlflow.log_metric("accuracy", accuracy)
         mlflow.log_metric("precision", precision)
         mlflow.log_metric("recall", recall)
         mlflow.log_metric("f1_score", f1)
         
-        # Log model
         logger.info("Logging model...")
         mlflow.sklearn.log_model(model, "random_forest_model")
         logger.info("Model logged successfully.")
     except Exception as e:
         logger.error(f"Error occurred: {e}")
-        raise  # Lempar error agar terlihat di log
+        raise
     
     logger.info(f"Accuracy: {accuracy:.4f}")
     logger.info(f"Precision: {precision:.4f}")
